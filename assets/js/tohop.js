ef4ae14f411be5a3c4ab87a22a98af0aa4e9d761
minhivan <minhmuofficial@gmail.com>
2020-07-28 08:27:36 +0700

    Edit

---
+++ assets/js/tohop.js
@@ -0,0 +1,63 @@
+var nvong = {
+    "7580201C" : {
+        "name" : "Kỹ thuật xây dựng (Chất lượng cao)",
+        "tohop": ["A00","A01"],
+        "id": 1
+    },
+
+    "7580201H" : {
+        "name" : "Kỹ thuật xây dựng - Học tại khu Hòa An",
+        "tohop" : ["A00","A01"],
+        "id": 2
+    },
+
+    "7380101" : {
+        "name" : "Luật",
+        "tohop" : ["B00","D01"],
+        "id": 3
+    },
+
+    "7380101H" : {
+        "name" : "Luật (Luật hành chính) - Học tại khu Hòa An",
+        "tohop" : ["B00","D01"],
+        "id": 4
+    },
+
+    "7340115" : {
+        "name" : "Marketing",
+        "tohop" : ["B00","D01"],
+        "id": 5
+    },
+    "7480102" : {
+        "name" : "Mạng máy tính và truyền thông dữ liệu",
+        "tohop" : ["A00","A01"],
+        "id": 6
+    }
+};
+var tohop = {
+    "A00" : {
+        "name" : "To hop A00",
+            "Mon": ["Toán","Lý","Hóa"],
+            "id": 1
+    },
+
+    "A01" : {
+        "name" : "To hop A01",
+            "tohop" : ["Toán","Lý","Anh văn"],
+            "id": 2
+    },
+
+    "B00" : {
+        "name" : "To hop B00",
+            "tohop" : ["Toán","Hóa","Sinh"],
+            "id": 3
+    },
+
+    "D07" : {
+        "name" : "To hop D01",
+            "tohop" : ["Toán","Văn","Anh văn"],
+            "id": 4
+    }
+};
+console.log(tohop["7480102"]);
+
