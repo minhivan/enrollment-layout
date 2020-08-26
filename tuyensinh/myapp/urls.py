aa1fff764d1130cada90d39a1b91d26191ccf94a
minhivan <minhmuofficial@gmail.com>
2020-08-26 10:32:45 +0700

    Update template + required'

--- tuyensinh/myapp/urls.py
+++ tuyensinh/myapp/urls.py
@@ -4,5 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login/', views.login),
-    path('apply/', views.apply),
+    path('register/', views.register),
+    path('apply', views.apply),
+    path('method/', views.method),
+    path('method/direct', views.direct),
+    path('method/graduate', views.graduate),
+    path('method/profile', views.checkProfile),
+    path('method/highquality', views.highQuality),
+    path('method/fostering', views.fostering),
+    path('method/pedagogy', views.pedagogy),
+    path('user/dashboard', views.clientDashboard),
 ]
--- tuyensinh/myapp/views.py
+++ tuyensinh/myapp/views.py
@@ -12,5 +12,42 @@ def login(request):
     return render(request, 'sign-in.html', {})
 
 
+def register(request):
+    return render(request, 'register.html', {})
+
+
+def clientDashboard(request):
+    return render(request, 'page/dashboard.html', {})
+
+
 def apply(request):
     return render(request, 'apply.html', {})
+
+
+def method(request):
+    return render(request, 'method.html', {})
+
+
+# Method page
+def direct(request):
+    return render(request, 'page/method1.html', {})
+
+
+def graduate(request):
+    return render(request, 'page/method2.html', {})
+
+
+def checkProfile(request):
+    return render(request, 'page/method3.html', {})
+
+
+def highQuality(request):
+    return render(request, 'page/method4.html', {})
+
+
+def fostering(request):
+    return render(request, 'page/method5.html', {})
+
+
+def pedagogy(request):
+    return render(request, 'page/method6.html', {})
